4463da470886485330fc20de9610902d8d17ea22
maratdzhukaev09 <60516734+maratdzhukaev09@users.noreply.github.com>
2020-07-13 19:30:53 +0300

    Add related_name to PokemonEntity

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -20,7 +20,7 @@ class Pokemon(models.Model):
 
 
 class PokemonEntity(models.Model):
-    pokemon = models.ForeignKey(Pokemon, on_delete=models.CASCADE)
+    pokemon = models.ForeignKey(Pokemon, on_delete=models.CASCADE, related_name="pokemon_entities")
     lat = models.FloatField()
     lon = models.FloatField()
 
--- pokemon_entities/views.py
+++ pokemon_entities/views.py
@@ -83,7 +83,7 @@ def show_pokemon(request, pokemon_id):
     }
 
     folium_map = folium.Map(location=MOSCOW_CENTER, zoom_start=12)
-    for pokemon_entity in PokemonEntity.objects.filter(pokemon=requested_pokemon):
+    for pokemon_entity in requested_pokemon.pokemon_entities.all():
         add_pokemon(
             folium_map,
             pokemon_entity.lat,
